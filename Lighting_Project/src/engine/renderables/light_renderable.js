f48179f39ac880a7f725719a84dc934b65af5e58
TylerTeifke <114880372+TylerTeifke@users.noreply.github.com>
2024-02-17 17:07:02 -0800

    Renamed the main folder and created the light_renderable class

---
+++ Lighting_Project/src/engine/renderables/light_renderable.js
@@ -0,0 +1,56 @@
+/*
+ * File: light_renderable.js
+ *
+ * Supports the drawing of an object with light sources attached to it
+*/
+"use strict";
+
+import TextureRenderable from "./texture_renderable.js";
+import * as shaderResources from "../core/shader_resources.js";
+
+class LightRenderable extends TextureRenderable{
+    constructor(myTexture){
+        //super._setShader(shaderResources.getLightShader());
+
+        //Will hold all light source objects
+        this.lightSources = [];
+    }
+
+    /**
+     * Will draw the light renderable object and all light sources
+     * @param {camera} camera 
+     */
+    draw(camera){
+        super.draw(camera);
+
+        //Will draw all light source on a light renderable object object
+        let i;
+        for(i = 0; i < this.lightSources.length; i++){
+            this.lightSources[i].draw(camera);
+        }
+    }
+
+    /**
+     * Will return the light source corresponding to the index
+     * @param {int} index 
+     */
+    getLightSource(index){
+        return this.lightSources[index];
+    }
+
+    /**
+     * Will append a light to the end of the lightSources array
+     * @param {lightSource} light 
+     */
+    addLightSource(light){
+        this.lightSources.push(light);
+    }
+
+    /**
+     * Will remove the light source in index
+     * @param {int} index 
+     */
+    removeLightSource(index){
+        this.lightSources.splice(index, 0);
+    }
+}
